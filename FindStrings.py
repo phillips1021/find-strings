9aa42d0050f02167c28c87ccb69f4e91a117886f
brucephillips <bphillip@stfm.org>
2020-01-12 10:19:42 -0600

    Code cleanup

--- FindStrings.py
+++ FindStrings.py
@@ -1,8 +1,10 @@
 TRADE = "TRADE"
 OFFTR = "OFFTR"
 UNKNOWN = "UNKNOWN"
-tradeRequiredTagFields = ['legTradeIds', 'receivedTime', 'selectTime']
-offtrRequiredTagFields = ['receivedTime', 'selectTime']
+trade_required_tagfields = ['legTradeIds', 'receivedTime', 'selectTime']
+offtr_required_tagfields = ['receivedTime', 'selectTime']
+
+# --------------Functions------------------------------------
 
 
 def get_line_type(line_str):
@@ -20,12 +22,39 @@ def get_line_type(line_str):
     return UNKNOWN
 
 
-with open('trades.txt', 'r') as tradesFile:
-    for line in tradesFile:
-        line_type = get_line_type(line)
-        if line_type == TRADE:
-            if not all(ele in line for ele in tradeRequiredTagFields):
-                print(f'NOT all trade required tagfields found in {line}')
-        if line_type == OFFTR:
-            if not all(ele in line for ele in offtrRequiredTagFields):
-                print(f'NOT all offtr required tagfields found in {line}')
+def get_trade_files():
+    """
+    Get list of trade file names
+    :return: list of trade file names
+    """
+    return ['trades.txt', 'trades2.txt']
+
+
+def validate_line(required_tagfields, trade_line, trade_file_name):
+    """
+    Check if required_tagfields are all present in the trade_line
+    and if all required tagfields are not present write to the log
+    :param required_tagfields: list of tagfields that must be present in trade_line
+    :param trade_line: line from trade file
+    :param trade_file_name: name of file
+    :return:
+    """
+    if not all(ele in trade_line for ele in required_tagfields):
+        print(f'NOT all required tagfields found in {line} from {trade_file_name}')
+
+
+# ----------------------Main Code--------------------------------
+
+trade_file_names = get_trade_files()
+
+for trade_file in trade_file_names:
+
+    with open(trade_file, 'r') as tradesFile:
+        for line in tradesFile:
+            line_type = get_line_type(line)
+            if line_type == TRADE:
+                validate_line(trade_required_tagfields, line, trade_file)
+                continue
+            if line_type == OFFTR:
+                validate_line(offtr_required_tagfields, line, trade_file)
+                continue
